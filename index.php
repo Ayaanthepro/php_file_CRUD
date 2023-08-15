70b74cccd31c07a1233682bc51c15ec6b0007872
Ayaanthepro <ayanrana7658@gmail.com>
2023-08-15 16:35:13 +0500

    dashboard

---
+++ index.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Dashboard</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+<style>
+     body {
+            height: 100vh;
+            overflow: hidden;
+            display: flex;
+            width: 100vw;
+            justify-content: center;
+            align-items: center;
+        }
+</style>
+</head>
+<body>
+    <div class="container">
+        <div class="row">
+            <div class="col-12">
+<div class="card">
+    <div class="card-body d-flex justify-content-center align-item-center">
+<a href="./create.php" class="btn btn-primary mx-3">Create User</a>
+<a href="./show_users.php" class="btn btn-success">Show Users</a>
+    </div>
+</div>
+            </div>
+        </div>
+    </div>
+</body>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
+
+</html>
